ad88bd5031c856775e2f5ebdc85e7f2a57b2c2f8
mdodge2 <mdodge@hawaii.edu>
2022-01-25 09:41:21 -1000

    add madis fetch, clean up dockerfiles/tasks

--- task-prelim/acquisition/hads-daily-fetch/task.sh
+++ task-prelim/acquisition/hads-daily-fetch/task.sh
@@ -6,5 +6,5 @@ cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/hads
 Rscript hads_24hr_webscape.R
 
 echo "[task.sh] All done! Moving on to the next step if there is one."
-# Continue the Workflow (momentarily removed)
+# Continue the Workflow (temporarily removed)
 #python3 /actor/run_next.py
--- task-prelim/acquisition/madis-daily-fetch/task.sh
+++ task-prelim/acquisition/madis-daily-fetch/task.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "[task.sh] Hello World! Ready to get today's hourly NWS data."
 
-# Execute Collection of NWS Hourly Data
-cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/nws_rr5
-Rscript nws_rr5_hrly_24hr_webscape.R
+# Execute Collection of MADIS Data
+cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/madis
+python3 mesonet_24hr_fetch_dev.py
+python3 hfmetar_24hr_fetch_dev.py
 
 echo "[task.sh] All done! Moving on to the next step if there is one."
 # Continue the Workflow (temporarily removed)
