5c41974c9c04e3eee153624a012924c554673d69
mdodge2 <mdodge@hawaii.edu>
2022-02-09 12:14:11 -1000

    aggregation skeleton

---
+++ task-prelim/aggregation/task.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "task.sh reporting! Ready to start working on Preliminary Daily Rainfall Acquisition."
+
+echo "This container isn't supposed to run by itself! Try appending -nws or -hads "
+
+# Continue the Workflow
+python3 /actor/run_next.py
