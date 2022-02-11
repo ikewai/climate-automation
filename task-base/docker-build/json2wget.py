ab14977d1a87bae8e9af1fb79178b1ed00ea4284
mdodge2 <mdodge@hawaii.edu>
2022-02-11 13:48:00 -1000

    hypothetically fixed builds using json2wget.py

--- task-base/docker-build/json2wget.py
+++ task-base/docker-build/json2wget.py
@@ -4,13 +4,9 @@ import sys, subprocess, json
 
 # Currently a prototype, needs to be improved for robustness/long-term security
 
-base_placement_dir = "/home/hawaii_climate_products_container/preliminary/data_aqs/code"
-
-if sys.argv[1] == None:
-    print("No arg! Exiting")
-    exit(1)
-elif str(sys.argv[1]) == "":
-    print("Arg Empty! Exiting")
+# Check for arguments
+if (sys.argv[1] == None) or (str(sys.argv[1]) == ""):
+    print("This script needs args! Pass it a json manifest.")
     exit(1)
 
 # Take arg 1 as the json file to read
@@ -21,6 +17,7 @@ with open(fname) as scripts_file:
 
 # var names and structure here are ugly, will fix later 🤞
 script_base_url: str = script_json["base_url"]
+script_placement_dir = script_json["placement_url"]
 script_list: list = script_json["scripts"]
 script_urls: list = script_list.copy()
 for i in range(len(script_urls)):
@@ -29,5 +26,5 @@ for i in range(len(script_urls)):
 
 
 for i in range(len(script_urls)):
-    command: str = f"wget {script_urls[i]} -O {base_placement_dir}/{script_list[i]}"
+    command: str = f"wget {script_urls[i]} -O {script_placement_dir}/{script_list[i]}"
     subprocess.run(["/bin/bash", "-c", command])
