307a8972c0e85a093be780d115213a071d5172d7
mdodge2 <mdodge@hawaii.edu>
2022-02-17 16:43:33 -1000

    workaround daily_temp_agg.sh

--- task-prelim/aggregation/airtemp/task.sh
+++ task-prelim/aggregation/airtemp/task.sh
@@ -3,7 +3,14 @@ echo "[task.sh] Hello World! Ready to aggregate daily Airtemp Data."
 
 # Run the aggregation workflow
 cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/code
-python3 daily_temp_agg.sh
+# the following will eventually be programmatic based on daily_temp_agg.sh
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_agg_wget.py
+
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/hads_temp_parse.py
+
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/madis_temp_parse.py
+
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/air_temp_aggregate_wrapper.py
 
 # Upload the aggregated data
 cd /sync
