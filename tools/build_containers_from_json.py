f44b49880c8fc2fb183e4db1cdf522e439b12b21
mdodge2 <mdodge@hawaii.edu>
2022-04-04 15:21:25 -1000

    add comments to build_containers_from_json.py

--- tools/build_containers_from_json.py
+++ tools/build_containers_from_json.py
@@ -1,4 +1,8 @@
 #!/bin/python3
+# This script builds docker containers from local files, as defined by a JSON file.
+# Arg 1: Path to the JSON file to read from.
+# Arg 2: Directory to log results to.
+
 
 import sys, subprocess, json
 
@@ -12,8 +16,8 @@ for container in container_list:
     build_dir: str = container["build_dir"]
     tag: str = ""
     for gh_repo in container["gh_repos"]:
-        tag = f'{tag}_{gh_repo["nickname"]}.{gh_repo["repo"]}'
+        tag = f'{tag}_{gh_repo["nickname"]}.{gh_repo["repo"]}' # Tag naming scheme
     
-    command: str = f"./build_container.sh {build_name}:{tag} {build_dir} {log_dir}"
+    command: str = f"./build_container.sh {build_name}:{tag} {build_dir} {log_dir}" # Pass arguments to build_container.sh
     subprocess.run("/bin/bash", "-c", command)
 
