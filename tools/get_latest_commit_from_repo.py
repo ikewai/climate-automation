ea606476305f383b6783942ed1904bbd43e2fb24
mdodge2 <mdodge@hawaii.edu>
2022-04-20 15:17:56 -1000

    deprecate clunky hash reader

--- tools/get_latest_commit_from_repo.py
+++ tools/get_latest_commit_from_repo.py
@@ -1,14 +1,3 @@
 #!/bin/python3
-# todo: add robustness to response handling
-
-import sys, requests, json
-
-repo: str = sys.argv[1] # Format: "ikewai/climate-automation"
-
-endpoint: str = f"https://api.github.com/repos/{repo}/commits" # Get commit log
-
-res = requests.get(endpoint)
-
-latest_commit: str = res.json()[0]['sha'][:8] # First eight chars of sha hash of latest comm.
-
-print(latest_commit)
+# DEPRECATED SCRIPT! USE `git rev-parse --short --verify main`
+# in shell after cloning the repo.
