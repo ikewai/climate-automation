acc487c18e0e977f1c66d27b8da42c4fc64a26b4
mdodge2 <mdodge@hawaii.edu>
2022-05-25 08:45:45 -1000

    remove leading /home/ in dir creation

--- task-prelim/aggregation/rainfall/txt2mkdir_rainfall.py
+++ task-prelim/aggregation/rainfall/txt2mkdir_rainfall.py
@@ -5,4 +5,4 @@ import sys, subprocess
 fin = open(sys.argv[1], "rt")
 
 for line in fin:
-    subprocess.run(["mkdir", "-p", f"/home/{str(line).strip()}"])
+    subprocess.run(["mkdir", "-p", f"{str(line).strip()}"])
