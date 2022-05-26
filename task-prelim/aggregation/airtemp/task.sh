4c14038eef8dfad515d3d6133664956b889da223
mdodge2 <mdodge@hawaii.edu>
2022-05-25 16:40:44 -1000

    disable upload on airtemp agg

--- task-prelim/aggregation/airtemp/task.sh
+++ task-prelim/aggregation/airtemp/task.sh
@@ -12,12 +12,12 @@ python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/d
 
 python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/air_temp_aggregate_wrapper.py
 
-# Upload the aggregated data
+# Upload the aggregated data TEMPORARILY DISABLED
 cd /sync
-python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json
-python3 upload_list_inserter.py upload_config_datestrings_loaded.json config.json
-python3 upload_auth_injector.py config.json
-python3 upload.py
+#python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json
+#python3 upload_list_inserter.py upload_config_datestrings_loaded.json config.json
+#python3 upload_auth_injector.py config.json
+#python3 upload.py
 
 echo "[task.sh] All done! Moving on to the next step if there is one."
 # Continue the Workflow (temporarily removed)
