d4089e74844e397675d7a84de9bae690c2953725
mdodge2 <mdodge@hawaii.edu>
2022-06-23 08:05:44 -1000

    enable upload

--- task-prelim/aggregation/airtemp-daily/task.sh
+++ task-prelim/aggregation/airtemp-daily/task.sh
@@ -17,7 +17,7 @@ cd /sync
 python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json
 python3 upload_list_inserter.py upload_config_datestrings_loaded.json config.json
 python3 upload_auth_injector.py config.json
-#python3 upload.py
+python3 upload.py
 
 echo "[task.sh] All done! Moving on to the next step if there is one."
 # Continue the Workflow (temporarily removed)
